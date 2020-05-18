94e50b9e6a5c0aee055143b1ca57e31e55ce3b61
Robson Silva <rms2307@outlook.com>
2020-05-18 15:01:57 -0300

    Piece Move Count

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -79,7 +79,7 @@ public class ChessMatch {
 		// Checking if opponent is in check mate
 		if (testCheckMate(opponent(currentPlayer))) {
 			checkMate = true;
-		}else {
+		} else {
 			nextTurn();
 		}
 
@@ -87,7 +87,8 @@ public class ChessMatch {
 	}
 
 	public Piece makeMove(Position source, Position target) {
-		Piece p = board.removePiece(source);
+		ChessPiece p = (ChessPiece) board.removePiece(source);
+		p.increaseMoveCount();
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
 
@@ -99,7 +100,8 @@ public class ChessMatch {
 	}
 
 	public void undoMove(Position source, Position target, Piece capturedPiece) {
-		Piece p = board.removePiece(target);
+		ChessPiece p = (ChessPiece) board.removePiece(target);
+		p.decreaseMoveCount();
 		board.placePiece(p, source);
 
 		if (capturedPiece != null) {
